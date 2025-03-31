beb9f6062fa4918825e98d7453763f5db1ead9c1
Rin <arina.averbakh.st@gmail.com>
2025-03-31 14:47:40 +0300

    Создан класс счётчик

---
+++ Counter.java
@@ -0,0 +1,32 @@
+class Counter implements AutoCloseable {
+    private int count;
+    private boolean isClosed = false;
+
+    public Counter() {
+        this.count = 0;
+    }
+
+    public void add() throws IllegalStateException {
+        if (isClosed) {
+            throw new IllegalStateException("Счётчик выключен. Невозможно добавить");
+        }
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (isClosed) {
+            throw new IllegalStateException("Счётчик уже выключен.");
+        }
+        isClosed = true;
+    }
+
+    public boolean isClosed() {
+        return isClosed;
+    }
+
+}
